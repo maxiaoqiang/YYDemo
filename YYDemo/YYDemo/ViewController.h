f1b85404b102f046c31cdf87ce6d80064475db96
maxiaoqiang <463502825@qq.com>
2017-04-24 09:57:04 +0800

    创建项目

---
+++ YYDemo/YYDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYDemo
+//
+//  Created by 深圳市泥巴装网络科技有限公司 on 2017/4/24.
+//  Copyright © 2017年 马晓强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YYDemo/YYDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYDemo
+//
+//  Created by 深圳市泥巴装网络科技有限公司 on 2017/4/24.
+//  Copyright © 2017年 马晓强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
