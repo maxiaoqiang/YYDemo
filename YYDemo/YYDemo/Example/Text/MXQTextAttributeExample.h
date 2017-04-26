210cf4e7d09a5eee43d4b7990f49a6e538f19060
maxiaoqiang <463502825@qq.com>
2017-04-26 14:16:33 +0800

    Attribute1

---
+++ YYDemo/YYDemo/Example/Text/MXQTextAttributeExample.h
@@ -0,0 +1,13 @@
+//
+//  MXQTextAttributeExample.h
+//  YYDemo
+//
+//  Created by 深圳市泥巴装网络科技有限公司 on 2017/4/25.
+//  Copyright © 2017年 马晓强. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface MXQTextAttributeExample : BaseController
+
+@end
--- YYDemo/YYDemo/Example/Text/TextController.m
+++ YYDemo/YYDemo/Example/Text/TextController.m
@@ -8,7 +8,12 @@
 
 #import "TextController.h"
 
-@interface TextController ()
+@interface TextController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *titles;
+@property (nonatomic, strong) NSMutableArray *classNames;
+
 
 @end
 
@@ -16,9 +21,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    self.titles = @[].mutableCopy;
+    self.classNames = @[].mutableCopy;
+    
+    [self addCell:@"Text Attributes 1" class:@"MXQTextAttributeExample"];
 }
+
+-(void)addCell:(NSString *)title class:(NSString *)className{
+    [self.titles addObject:title];
+    [self.classNames addObject:className];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _titles.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"YY"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"YY"];
+        
+    }
+    
+    cell.textLabel.text = _titles[indexPath.row];
+    return  cell;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *className = self.classNames[indexPath.row];
+    Class class = NSClassFromString(className);
+    if (class) {
+        UIViewController *ctrl = class.new;
+        ctrl.title = _titles[indexPath.row];
+        [self.navigationController pushViewController:ctrl animated:YES];
+    }
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+
+
+
 
 
 
